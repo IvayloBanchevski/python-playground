2fb76d87fa5eadf78e6cceaf6beb6c544b1597bc
Ivaylo Banchevski <ivaylo@dagroup.bg>
2016-10-12 22:52:27 +0300

    More exercies.

---
+++ week1/compare.py
@@ -0,0 +1,11 @@
+# Get two numbers from user
+numb1 = int(input("First number: "))
+numb2 = int(input("Second number: "))
+
+# Compare
+if (numb1 > numb2):
+    print("Numb1 is higher than numb2")
+elif (numb2 > numb1):
+    print("Numb2 is higher than numb1")
+else:
+    print("The numbers are even.")
---
+++ week1/friday.py
@@ -0,0 +1,8 @@
+import time
+
+today = time.strftime("%A")
+
+if (today == "Friday"):
+    print("TODAY IS FRIDAY!!! YAYYYY!!!")
+else:
+    print("Today is not Friday :(. Today is " + today)
